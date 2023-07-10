9848122ad00a968c39a4871d170cd87690bc25cd
Marwin Wongjarupun <marwin111@gmail.com>
2023-07-10 19:17:16 +0700

    added successful subscription message

--- out/assets/js/subscribe.js
+++ out/assets/js/subscribe.js
@@ -1,10 +1,17 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycby6IY6g_cgXupQdwfZmXDoUXwMb0jESUI9Zu4OufxNer6aAbPURtulP02xqDd3u_p-p/exec'
+const form = document.forms['subscribers']
+const msg = document.getElementById("msg")
 
-  const scriptURL = 'https://script.google.com/macros/s/AKfycby6IY6g_cgXupQdwfZmXDoUXwMb0jESUI9Zu4OufxNer6aAbPURtulP02xqDd3u_p-p/exec'
-  const form = document.forms['subscribers']
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        console.log('Success!', response)
+        msg.innerHTML= "Thank you for subscribing!"
+        setTimeout(function () {
+            msg.innerHTML= ""
+        }, 5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
