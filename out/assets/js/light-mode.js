149d9453a1905b892494e9808d2628415fdf416c
Marwin Wongjarupun <marwin111@gmail.com>
2023-09-10 14:16:51 +0700

    changed so that light/dark mode icons are in correct mode depending on the state of the website

--- out/assets/js/light-mode.js
+++ out/assets/js/light-mode.js
@@ -1,32 +1,39 @@
+// Function to toggle between light and dark mode
+function toggleLightMode() {
+    // Check the current mode by inspecting the body's class or style
+    const body = document.body;
+    const isLightMode = body.classList.contains('light-mode');
+  
+    // Toggle the current mode
+    const newMode = !isLightMode;
+
+    // Apply the selected mode
+    if (newMode) {
+        // Switch to light mode
+        body.classList.add('light-mode');
+        // Change the button icon to moon (dark mode)
+        document.getElementById('toggle-theme-button').innerHTML = '<i class="fas fa-moon"></i>';
+    } else {
+        // Switch to dark mode
+        body.classList.remove('light-mode');
+        // Change the button icon to sun (light mode)
+        document.getElementById('toggle-theme-button').innerHTML = '<i class="fas fa-sun"></i>';
+    }
+
+    // Update the preference in local storage
+    localStorage.setItem('lightMode', newMode.toString());
+}
+
 // Check if a light mode preference is stored in local storage
 let isLightMode = localStorage.getItem('lightMode') === 'true';
 
-// Function to toggle between light and dark mode
-function toggleLightMode() {
-  // Toggle the current mode
-  isLightMode = !isLightMode;
-
-  // Apply the selected mode
-  if (isLightMode) {
-    // Switch to light mode
-    document.body.classList.add('light-mode');
-    // Change the button icon to moon (dark mode)
-    document.getElementById('toggle-theme-button').innerHTML = '<i class="fas fa-moon"></i>';
-  } else {
-    // Switch to dark mode
-    document.body.classList.remove('light-mode');
-    // Change the button icon to sun (light mode)
-    document.getElementById('toggle-theme-button').innerHTML = '<i class="fas fa-sun"></i>';
-  }
-
-  // Update the preference in local storage
-  localStorage.setItem('lightMode', isLightMode.toString());
-}
-
 // Apply light mode if the preference is set
 if (isLightMode) {
-  document.body.classList.add('light-mode');
+    document.body.classList.add('light-mode');
 }
+
+// Initialize the button icon based on the initial mode
+document.getElementById('toggle-theme-button').innerHTML = isLightMode ? '<i class="fas fa-moon"></i>' : '<i class="fas fa-sun"></i>';
 
 // Call the function when the button is clicked
 document.getElementById('toggle-theme-button').addEventListener('click', toggleLightMode);
