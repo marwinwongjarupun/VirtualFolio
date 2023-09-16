8c64a9c215f53a5e4071df5c57fa3fc5e24df139
Marwin Wongjarupun <marwin111@gmail.com>
2023-09-16 20:15:48 +0700

    added new symbols

--- out/assets/js/light-mode.js
+++ out/assets/js/light-mode.js
@@ -1,5 +1,4 @@
 // Function to toggle between light and dark mode
-
 function toggleLightMode() {
     // Check the current mode by inspecting the body's class or style
     const body = document.body;
@@ -13,7 +12,7 @@ function toggleLightMode() {
         // Switch to light mode
         body.classList.add('light-mode');
         // Change the button icon to moon (dark mode)
-        document.getElementById('toggle-theme-button').innerHTML = '<i class="fas fa-solid fa-lightbulb"></i>';
+        document.getElementById('toggle-theme-button').innerHTML = '<i class="material-icons">dark_mode</i>';
         // Change the logo image to the light version
         document.querySelectorAll('.logo-img').forEach(function(logoImg) {
             logoImg.src = '/assets/img/logo-light-new.png'; // Replace with the correct image path
@@ -22,7 +21,7 @@ function toggleLightMode() {
         // Switch to dark mode
         body.classList.remove('light-mode');
         // Change the button icon to sun (light mode)
-        document.getElementById('toggle-theme-button').innerHTML = '<i class="fas fa-regular fa-lightbulb"></i>';
+        document.getElementById('toggle-theme-button').innerHTML = '<span class="material-symbols-outlined">light_mode</span>';
         // Change the logo image to the dark version
         document.querySelectorAll('.logo-img').forEach(function(logoImg) {
             logoImg.src = '/assets/img/logo-dark-new.png'; // Replace with the correct image path
@@ -46,7 +45,7 @@ if (isLightMode) {
 }
 
 // Initialize the button icon based on the initial mode
-document.getElementById('toggle-theme-button').innerHTML = isLightMode ? '<i class="fas fa-solid fa-lightbulb"></i>' : '<i class="fas fa-regular fa-lightbulb"></i>';
+document.getElementById('toggle-theme-button').innerHTML = isLightMode ? '<i class="material-icons">dark_mode</i>' : '<span class="material-symbols-outlined">light_mode</span>';
 
 // Call the function when the button is clicked
 document.getElementById('toggle-theme-button').addEventListener('click', toggleLightMode);
