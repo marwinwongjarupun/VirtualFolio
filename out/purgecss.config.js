4a094b4ae36a84a962d5342d1e83c4c943ca335c
Marwin Wongjarupun <marwin111@gmail.com>
2023-09-19 04:34:14 +0700

    added purgecss

--- out/purgecss.config.js
+++ out/purgecss.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     content: ["_site/**/*.html", "_site/**/*.js"], // Paths to your generated HTML and JavaScript files
+    css: ["_site/**/*.css"],
     // Other PurgeCSS options can be specified here
 };
