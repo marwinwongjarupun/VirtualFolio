5be95836d58a6eeb00cc50b47c589a07aeb55c26
Marwin Wongjarupun <marwin111@gmail.com>
2024-06-25 01:10:50 +0100

    changed project id

--- out/assets/js/firebase.js
+++ out/assets/js/firebase.js
@@ -2,20 +2,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
-import { getPerformance } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-performance.js";
+import { getPerformance } from "firebase/performance";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGMA88fUw3tRvw-IPaZn6aoG6GfxevUDM",
-  authDomain: "marwinwongjarupun.dev",
-  databaseURL: "https://virtual-folio-212c2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "virtual-folio-212c2",
-  storageBucket: "virtual-folio-212c2.appspot.com",
-  messagingSenderId: "243149484747",
-  appId: "1:243149484747:web:160e963951e2ae025ea14e",
-  measurementId: "G-F4YG2QY3CQ"
+  apiKey: "AIzaSyA3q_6u23Cv19qmXC_VAQyHUhljqcrtcAY",
+  authDomain: "marwinwongjarupun.firebaseapp.com",
+  projectId: "marwinwongjarupun",
+  storageBucket: "marwinwongjarupun.appspot.com",
+  messagingSenderId: "1066049890889",
+  appId: "1:1066049890889:web:893e0cb09356ee5ce22d98",
+  measurementId: "G-VJCQVS8X8J"
 };
 
 // Initialize Firebase
